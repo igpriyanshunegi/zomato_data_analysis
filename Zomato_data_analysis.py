c8b25a59dad9141ed3db539345cf202b382aca5b
igpriyanshunegi <igpriyanshunegi@gmail.com>
2025-08-18 10:03:14 +0530

    Data Cleaning and Preparation

--- Zomato_data_analysis.py
+++ Zomato_data_analysis.py
@@ -6,3 +6,13 @@ import seaborn as sns
 # Load the dataset
 dataframe = pd.read_csv('zomato-data.csv')
 print(dataframe.head())
+
+# Data Cleaning and Preparation
+def handleRate(value):
+    value=str(value).split('/')
+    value=value[0];
+    return float(value)
+
+dataframe['rate']=dataframe['rate'].apply(handleRate)
+print(dataframe.head())
+
